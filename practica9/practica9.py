88781501a1685d37964a951c374cdacb2487c951
solmarin <55603215+solmarin@users.noreply.github.com>
2019-11-27 18:50:09 +0100

    Add files via upload

--- practica9/practica9.py
+++ practica9/practica9.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from multiprocessing import Pool
 def primers(num):
+    """ PROGRAMA PARA EJECTURAR DIFERENTES PROCESOS A LA VEZ
+    """
+
     for i in range(2, num/2):
         if num % i == 0:
             return False
@@ -23,6 +26,9 @@ if __name__ == '__main__':
         i+=1
 #Mostrar el tiempo total de todos los procesos
     print datetime.now() - start
+"""
+mejor ejecutar alrededor de 10 procesos ( son los que ejecutan más rapidos) a la vez pero más no porque el ordenador no tiene recursos para ejectuarlos y deja de funcionar
+"""
 
 #TIEMPOS FINALES
 #10 [0:37.811796]
