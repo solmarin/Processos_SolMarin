ff3f2372ff7f93cf7d6fd11251e99d06d0de8fb1
solmarin <55603215+solmarin@users.noreply.github.com>
2020-01-14 19:30:37 +0100

    Update client-speak.py

--- practica15/client-speak.py
+++ practica15/client-speak.py
@@ -5,10 +5,9 @@ import sys
 HOST = 'localhost'    # The remote host
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect((HOST,PORT))
 while True:
     print >> sys.stderr, 'Introduce palabras:'
     data = raw_input()
-    s.sendall(data)
+    s.sendall(data,((HOST,PORT)))
     if(data=='Bye'): break
 s.close()
